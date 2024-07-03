16859690713ffde22895a0c775013d83e79140b1
Ryan Sheehan <rsheehan@gmail.com>
2024-07-03 15:10:35 -0500

    inc commit

---
+++ src/lib/components/app-bar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import LogoutButton from '$lib/components/logout-button.svelte';
+
+    type AppBarProps = {
+        isUserLoggedIn: boolean;
+    }
+
+    let {isUserLoggedIn}: AppBarProps = $props();
+</script>
+
+<header class="content-grid">    
+    <nav class="breakout">
+        <a href="/">Home</a>
+        <a href="/public">Public</a>
+        {#if isUserLoggedIn}
+        <a href="/protected">Protected</a>        
+        {/if}
+    </nav>
+    <div class="right-side">
+        {#if isUserLoggedIn}
+        <LogoutButton/>
+        {:else}
+        <a href="/login">Login</a>
+        {/if}
+    </div>    
+</header>
+
+<style>
+    
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
-    import Logout from '$lib/components/logout.svelte';
+    import AppBar from '$lib/components/app-bar.svelte';
     import "../app.css";
 
     let {children, data} = $props();   
     let isUserLoggedIn = $derived(data.isUserLoggedIn); 
 </script>
 
-<nav>
-    <a href="/">Home</a>
-    <a href="/public">Public</a>
-    {#if isUserLoggedIn}
-    <a href="/protected">Protected</a>    
-    <Logout/>
-    {:else}
-    <a href="/login">Login</a>
-    {/if}
-</nav>
+<AppBar {isUserLoggedIn} />
 
 <main>
     {@render children()}
